71e1038e375f7b3cc7cb3885319df541708d7b91
Gustavo <gustavocoimbra04@gmail.com>
2021-08-02 19:27:57 -0300

    CriacaoBD_Teste1

---
+++ config.php
@@ -0,0 +1,51 @@
+<?php
+class BancodeDados{
+    private $host = "localhost";
+    private $user = "root";
+    private $senha = "";
+    private $banco = "escola_tcc";
+    public $con;
+    
+    function conecta(){
+    $this->con = @mysqli_connect($this->host,$this->user,$this->senha, $this->banco);
+        if(!$this->con){
+
+            die ("Problemas com a conexăo");
+        }
+    }   
+    function fechar(){
+        mysqli_close($this->con);
+        return;
+    }
+    function sqlquery($string,$caminho){
+        $resultado = @mysqli_query($this->con, $string);
+        if (!$resultado) {
+            echo '<input type="button" onclick="window.location='."'index.php'".';" value="Voltar"><br><br>';
+            die('<b>Query Inválida:</b>' . @mysqli_error($this->con)); 
+        } else {
+            $num = @mysqli_num_rows($resultado);
+            if ($num==0){
+            echo "<b>Código: </b>năo localizado !!!!<br><br>";
+            echo '<input type="button" onclick="window.location='."'$caminho'".';" value="Voltar"><br><br>';
+            exit;       
+            }else{
+                $dados=mysqli_fetch_array($resultado);
+            }
+        } 
+        $this->fechar();
+        return $dados;
+    }
+    function sqlstring($string,$texto){
+        $resultado = @mysqli_query($this->con, $string);
+        if (!$resultado) {
+            echo '<input type="button" onclick="window.location='."'index.php'".';" value="Voltar"><br><br>';
+            die('<b>Query Inválida:</b>' . @mysqli_error($mysql->con)); 
+        } else {
+            echo "<b>$texto </b> - Realizada com  Sucesso";
+        }
+        $this->fechar();
+        return;
+    }
+    
+}
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCKTYPE html>
+<html>
+    <head>
+     <meta charset="UTF-8">
+     <meta description="...">
+    </head>
+     <body>
+                 
+
+
+
+
+
+
+     </body>
+</html>
