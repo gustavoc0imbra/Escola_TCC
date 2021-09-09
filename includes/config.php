60aaafc6854cb106dcce8c2753e84568af03f0a7
TiagoCostaSantos <tiagocosta7603@gmail.com>
2021-09-09 15:19:25 -0300

    BD

--- includes/config.php
+++ includes/config.php
@@ -1,51 +1,19 @@
 <?php
-class BancodeDados{
-    private $host = "localhost";
-    private $user = "root";
-    private $senha = "";
-    private $banco = "escola_tcc";
-    public $con;
-    
-    function conecta(){
-    $this->con = @mysqli_connect($this->host,$this->user,$this->senha, $this->banco);
-        if(!$this->con){
 
-            die ("Problemas com a conexăo");
-        }
-    }   
-    function fechar(){
-        mysqli_close($this->con);
-        return;
-    }
-    function sqlquery($string,$caminho){
-        $resultado = @mysqli_query($this->con, $string);
-        if (!$resultado) {
-            echo '<input type="button" onclick="window.location='."'index.php'".';" value="Voltar"><br><br>';
-            die('<b>Query Inválida:</b>' . @mysqli_error($this->con)); 
-        } else {
-            $num = @mysqli_num_rows($resultado);
-            if ($num==0){
-            echo "<b>Código: </b>năo localizado !!!!<br><br>";
-            echo '<input type="button" onclick="window.location='."'$caminho'".';" value="Voltar"><br><br>';
-            exit;       
-            }else{
-                $dados=mysqli_fetch_array($resultado);
-            }
-        } 
-        $this->fechar();
-        return $dados;
-    }
-    function sqlstring($string,$texto){
-        $resultado = @mysqli_query($this->con, $string);
-        if (!$resultado) {
-            echo '<input type="button" onclick="window.location='."'index.php'".';" value="Voltar"><br><br>';
-            die('<b>Query Inválida:</b>' . @mysqli_error($mysql->con)); 
-        } else {
-            echo "<b>$texto </b> - Realizada com  Sucesso";
-        }
-        $this->fechar();
-        return;
-    }
-    
+/* linha de conexão com BD
+$banco = new mysqli(host,usuario,senha,banco);*/
+$banco = new mysqli("localhost","root","","escola_tcc");
+
+/* Se houver algum erro na conexão será emitido uma msg de die() mata tudo*/
+if($banco->connect_errno){
+    echo"<p>Encontrei um erro $banco->errno --> $banco->connect_error</p>";
+    die();
 }
+
+/* Transformar os resultados com padrões utf-8 */
+$banco->query("SET NAMES 'utf8'");
+$banco->query("SET character_set_connection=utf8");
+$banco->query("SET character_set_client=utf8");
+$banco->query("SET character_set_results=utf8");
+
 ?>
