ce79f25151c470fff80a85d6079846b4e15927e5
FALLslo <joaozinhopedefeihao828@gmail.com>
2021-09-11 13:11:18 -0300

    Eu criei a pag notas, porem, precisei conectar-la com outro conexão( BD.php). Depois, voces substituem lá no notas.php pelo config.php

--- Escola_TCC.sql
+++ Escola_TCC.sql
@@ -10,3 +10,22 @@ alter table administrador modify senhaAdm varchar(60);
 alter table responsaveis modify senhaAluno varchar(60);
 alter table responsaveis add nomeResponsavel varchar(60) after senhaAluno;
 alter table responsaveis add senhaResponsavel varchar(60) after nomeResponsavel;
+
+
+/* alguns insert para testar a tabela notas */
+INSERT INTO armazinfo (notaAluno, mediaBimestre, frequenciaAluno, FKcodDisciplina)
+VALUES (5, 7,0.89,1);
+
+INSERT INTO armazinfo (notaAluno, mediaBimestre, frequenciaAluno, FKcodDisciplina)
+VALUES (8, 5 ,0.3,1);
+
+INSERT INTO armazinfo (notaAluno, mediaBimestre, frequenciaAluno, FKcodDisciplina)
+VALUES (4, 4,0.52,2);
+
+
+
+
+DELETE FROM armazinfo
+WHERE notaAluno = 4.2;
+
+select * from armazinfo;
