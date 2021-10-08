583ac2969be1ca8d4f0fe77e27e33bb571eacf2e
TiagoCostaSantos <tiagocosta7603@gmail.com>
2021-10-08 14:32:33 -0300

    Commit alterar dados + ver usuarios + navbar

---
+++ user_edit.php
@@ -0,0 +1,231 @@
+<docytipe !html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <?php 
+        require_once "includes/config.php";
+        require_once "includes/functions.php";
+        $tipo = $_SESSION['tipo']?? null;
+    ?>
+    <body>
+    <?php 
+        $edit = null;
+        $delete = null;
+        if($tipo == "admin"){
+            
+//          variaveis do user_view
+            $edit = "true";
+            $c = $_GET['tipoUsuario']?? null;
+            $cod = $_GET['cod']?? null;
+            $nome = $_GET['nome']?? null;
+            $delete = $_GET['delete']?? null;
+
+            echo "<h1>Alterar dados do $c </h1>";
+        }
+        
+            
+            if($delete == "true"){
+                    
+                    if($c == 'aluno'){
+                        
+                        $q= "SELECT codResponsavel FROM responsaveis where codAluno = $cod;";
+                        
+                        $busca= $banco->query($q);
+                        $reg = $busca->fetch_object();
+                        $responsavel = $reg->codResponsavel??null;
+                        
+                        if($responsavel == null){
+                        
+                            $q = "DELETE FROM estudante WHERE codAluno=$cod";
+                            if($banco->query($q)){
+                                echo "Usuário deletado com sucesso!";
+
+                            }else{
+                                echo "Erro ao deletar aluno, Tente novamente mais tarde!";
+                            }
+
+                            echo"<br><br><a href='user_view.php?tipoSelect=aluno'>voltar</a>";
+                        }else{
+                            $q = "DELETE FROM responsaveis where codAluno = $cod";
+                            
+                            if($banco->query($q)){
+                                
+                                $q = "DELETE FROM estudante WHERE codAluno=$cod";
+                            if($banco->query($q)){
+                                echo "Usuário deletado com sucesso!";
+
+                            }else{
+                                echo "Erro ao deletar aluno, Tente novamente mais tarde!";
+                            }
+                                
+                            }else{
+                                echo "Erro ao apagar o responsavel do aluno";
+                            }
+                        }
+                        
+                    }else if($c == 'professor'){
+                        
+                        $q = "DELETE FROM professor WHERE codProf = $cod";
+                        if($banco->query($q)){
+                            echo "Usuário deletado com sucesso!";
+                            
+                        }else{
+                            echo "Erro ao deletar aluno, Tente novamente mais tarde!";
+                        }
+                        
+                        echo "<br><a href='user_view.php?tipoSelect=professor'>voltar</a>";
+                        
+                    }else if ($c == 'admin'){
+                        
+                        $q = "DELETE FROM administrador WHERE codAdm = $cod";
+                        if($banco->query($q)){
+                            logout();
+                            echo "Usuário deletado com sucesso!, por favor faça novamente o <a href='login.php'>login</a>";
+                            
+                        }else{
+                            echo "Erro ao deletar admin, Tente novamente mais tarde!";
+                            
+                        }
+                        
+                        
+                    }else{
+                        
+                        $q = "DELETE FROM responsaveis WHERE codResponsavel = $cod";
+                        if($banco->query($q)){
+                            echo "Usuário deletado com sucesso!";
+                            
+                        }else{
+                            echo "Erro ao deletar aluno, Tente novamente mais tarde!";
+                        }
+                        
+                         echo"<br><br><a href='user_view.php?tipoSelect=responsavel'>voltar</a>";
+                        
+                    }  
+        }else{
+        
+            if(!isset($_POST['usuario'])){
+                include "user_edit_form.php";
+            }else{
+
+                $codNovo = $_POST['usuario']?? null;
+                $nome = $_POST['nome']?? null;
+                $senha1 = $_POST['senha1']?? null;
+                $senha2 = $_POST['senha2']?? null;
+                $datanasc = $_POST['datanasc']?? null;
+                $rg = $_POST['rg']?? null;
+                $cpf = $_POST['cpf']?? null;
+                $cel = $_POST['cel']?? null;
+                $disciplina = $_POST['disciplina']?? null;
+                $tel = $_POST['tel']?? null;
+                $cidade = $_POST['cidade']?? null;
+                $bairro = $_POST['bairro']?? null;
+                $rua = $_POST['rua']?? null;
+                $rmAluno = $_POST['rmAluno']?? null;
+
+                if($tipo != "admin"){
+
+                    $c = $tipo;
+                    $cod = $_SESSION['user'];
+
+                }else{
+
+                    $c = $_GET['c']??null;
+                    $cod = $_GET['cod']??null;
+                }
+
+
+                if($c == "admin"){
+
+                    $q = "UPDATE administrador SET codAdm = '$codNovo', nomeAdm = '$nome'";
+
+                    if($senha1 != null){
+
+                        if($senha1 == $senha2){
+
+                            $senha=gerarhash($senha1);
+                            $q.= " , senhaAdm='$senha'";
+
+                            }else{
+                                echo "Senhas não conferem a senha anterior será mantida!";
+                        }   
+                    }
+
+                    $q.="WHERE codAdm='$cod';";
+
+                }else if($c == "aluno"){
+
+                    $q = "UPDATE estudante SET codAluno='$codNovo', nomeAluno='$nome', datanascAluno='$datanasc', RG='$rg', CPF='$cpf', telefoneAluno='$tel', bairroAluno='$bairro', ruaAluno='$rua', cidadeAluno='$cidade'";
+
+                    if($senha1 != null){
+                        if($senha1 == $senha2){
+
+                            $senha=gerarhash($senha1);
+                            $q.= " , senhaAluno='$senha'";
+
+                        }else{
+                            echo "Senhas não conferem a senha anterior será mantida!";
+                        }
+                    }
+
+                    $q.=" WHERE codAluno='$cod';";
+
+                }else if($c == "professor"){
+
+                    $q = "UPDATE professor SET codProf='$codNovo', nomeProf='$nome', datanascProf='$datanasc', celProf='$cel', telProf='$tel', bairroProf='$bairro', ruaProf='$rua', cidadeProf='$cidade', codDisciplina='$disciplina'";
+
+                    if($senha1 != null){
+                        if($senha1 == $senha2){
+
+                            $senha=gerarhash($senha1);
+                            $q.= " , senhaProf='$senha'";
+
+                        }else{
+                            echo "Senhas não conferem a senha anterior será mantida!";
+                        }
+                    }
+
+                    $q.=" WHERE codProf='$cod';";
+
+
+                }else if($c == "responsavel"){
+
+                    $q = "UPDATE responsaveis SET codResponsavel='$codNovo', nomeResponsavel='$nome', cpfMae='$cpf', rgMae='$rg', codAluno='$rmAluno'";
+
+                    if($senha1 != null){
+                        if($senha1 == $senha2){
+
+                            $senha=gerarhash($senha1);
+                            $q.= " , senhaResponsavel='$senha'";
+
+                        }else{
+                            echo "Senhas não conferem a senha anterior será mantida!";
+                        }
+                    }
+
+                    $q.=" WHERE codResponsavel='$cod';";
+
+                }
+
+                if($banco->query($q)){
+                    echo "Dados alterado com sucesso!";
+                    
+                    if($edit != "true"){
+                        
+                       logout();
+                       echo "<br>por favor faça o login novamente!";
+                    }
+
+                }else{
+                    echo "Não foi possivel alterar os dados, tente novamente mais tarde :/";
+                }
+
+            }
+        }
+        
+        
+    ?>
+    
+        <Br><br><a href="index.php">Menu</a>
+        
+    </body>
---
+++ user_view.php
@@ -0,0 +1,221 @@
+<docytipe !html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            table {
+              font-family: arial, sans-serif;
+              border-collapse: collapse;
+              width: 100%;
+            }
+
+            td, th {
+              border: 1px solid grey;
+              text-align: left;
+              padding: 5px;
+            }
+
+            tr:nth-child(even) {
+              background-color: #dddddd;
+            }
+        </style>
+    </head>
+    <?php 
+        require_once "includes/config.php";
+        require_once "includes/functions.php";
+        $n = $_GET['tipoSelect']?? null;
+        $tipo = $_SESSION['tipo']?? null;
+    ?>
+    <body>
+        <h1>Usuários <?php echo "$n" ?></h1>
+        <?php
+            if($tipo == "admin"){
+                echo "<a href='cadastro.php'>Novo usuário</a> ||";
+                
+//                    Obtendo menor e maior valor das tabelas de usuarios
+                if($n == "aluno"){           
+                    $q = "SELECT MIN(codAluno) AS menorCod, MAX(codAluno) AS maiorCod FROM estudante;";
+                    
+                }else if($n == "professor"){
+                    $q = "SELECT MIN(codProf) AS menorCod, MAX(codProf) AS maiorCod FROM professor;";
+                
+                }else{
+                    $q = "SELECT MIN(codResponsavel) AS menorCod, MAX(codResponsavel) AS maiorCod FROM responsaveis;";
+                }
+                    $buscaCod = $banco->query($q);
+                    $reg = $buscaCod->fetch_object();
+                    $c = $reg->menorCod;
+                    $maiorCod = $reg->maiorCod;
+                
+                if($banco->query($q)){
+                    
+                    if(!$reg->menorCod == null){
+                        
+                        if($n == "aluno"){
+                            ?>
+                            <a href='user_view.php?tipoSelect=responsavel'>Usuários responsavel  ||</a>
+                            <a href='user_view.php?tipoSelect=professor'>Usuários professores </a><br><Br>
+                            <table>
+                                <tr>
+                                    <th>Rm</th>
+                                    <th>Nome</th>
+                                    <th>Foto</th>
+                                    <th>Data de nasc</th>
+                                    <th>CPF</th>
+                                    <th>RG</th>
+                                    <th>telefone</th>
+                                    <th>Endereço</th>
+                                    <th></th>
+                                </tr>
+                        <?php
+                         
+                        while($c !=$maiorCod+1){
+                            
+                            $q = "SELECT * FROM estudante WHERE codAluno = $c";
+                            $busca = $banco->query($q);
+                            
+                            if($banco->query($q)){
+                                $reg = $busca->fetch_object();
+                                $regEmpty = $reg->codAluno?? null;
+                                
+                                if($regEmpty == null ){
+                                    $c++;
+                                }else{
+                                
+                                    
+//                                dados mostrados dentro da tabela 'usuarios aluno'
+                                echo"<tr>
+                                        <td>$reg->codAluno</td>
+                                        <td>$reg->nomeAluno</td>
+                                        <td>$reg->imagemEstudante</td>
+                                        <td>$reg->datanascAluno</td>
+                                        <td>$reg->CPF</td>
+                                        <td>$reg->RG</td>
+                                        <td>$reg->telefoneAluno</td>
+                                        <td>$reg->cidadeAluno,$reg->bairroAluno,<br>$reg->ruaAluno</td>
+                                        <td><a href='user_edit.php?tipoUsuario=aluno&nome=$reg->nomeAluno&cod=$reg->codAluno'>alterar dados</a></td>
+                                    </tr>";
+                                $c ++;
+                                }
+                                
+                            }else{
+                                echo "Erro na tabela do banco de dados! tente novamente mais tarde ;/";
+                            }
+                        }echo "</table>";
+                            
+//                      tabela dos professores
+                        }else if($n == "professor"){
+                            ?> 
+                            <a href="user_view.php?tipoSelect=aluno">Usuários alunos ||</a>
+                            <a href='user_view.php?tipoSelect=responsavel'>Usuários responsavel  ||</a><br><Br>
+                            <table>
+                                <tr>
+                                    <th>Rm</th>
+                                    <th>Nome</th>
+                                    <th>Data de nasc</th>
+                                    <th>Tel/Cel</th>
+                                    <th>Endereço</th>
+                                    <th>Disciplina</th>
+                                    <th>Foto</th>
+                                    <th>Alterar</th>
+
+                                </tr>
+                            <?php     
+                            while($c !=$maiorCod+1){
+                                
+                                $q = "SELECT * FROM professor WHERE codProf = $c";
+                                $busca = $banco->query($q);
+                                
+                                if($banco->query($q)){
+                                    $reg= $busca->fetch_object();
+                                    $regEmpty = $reg->codProf?? null;
+                                    
+                                    if($regEmpty == null){
+                                        $c++;
+                                    }else{
+//                                      dados mostrados dentro da tabela 'usuarios professores'
+                                        echo "<tr>
+                                                <td>$reg->codProf</td>
+                                                <td>$reg->nomeProf</td>
+                                                <td>$reg->datanascProf</td>
+                                                <td>$reg->telProf<br>$reg->celProf</td>
+                                                <td>$reg->cidadeProf<br>$reg->bairroProf<br>$reg->ruaProf</td> 
+                                                <td>$reg->codDisciplina</td> 
+                                                <td>$reg->imagemProfessor</td>  
+                                                <td><a href='user_edit.php?tipoUsuario=professor&nome=$reg->nomeProf&cod=$reg->codProf'>alterar dados</a></td> 
+                                              </tr>";
+                                        $c++;
+                                    }
+                                    
+                                }else{
+                                     echo "Erro na tabela do banco de dados! tente novamente mais tarde ;/";
+                                }
+                                
+                            }echo "</table>";
+                            
+//                      tabela dos responsaveis
+                        }else{
+                            ?>
+                             <a href="user_view.php?tipoSelect=aluno">Usuários alunos ||</a>
+                             <a href='user_view.php?tipoSelect=professor'>Usuários professores </a><br><Br>
+                             <table>
+                                 <tr>
+                                     <th>Rm</th> 
+                                     <th>Nome</th>
+                                     <th>CPF</th>
+                                     <th>Rg</th>
+                                     <th>Responsavel de:</th>
+                                     <th>Alterar</th>
+
+                                 </tr>
+                            <?php
+                            while($c !=$maiorCod+1){
+                                
+                                $q = "SELECT * FROM responsaveis WHERE codResponsavel = $c";
+                                $busca = $banco->query($q);
+                                
+                                if($banco->query($q)){
+                                    $reg= $busca->fetch_object();
+                                    $regEmpty = $reg->codResponsavel?? null;
+                                    
+                                    if($regEmpty == null){
+                                        $c++;
+                                        echo "teste";
+                                    }else{
+//                                      dados mostrados dentro da tabela 'usuarios responsaveis'
+                                        echo "<tr>
+                                                <td>$reg->codResponsavel</td>
+                                                <td>$reg->nomeResponsavel</td>
+                                                <td>$reg->cpfMae</td>
+                                                <td>$reg->rgMae</td>
+                                                <td>$reg->codAluno</td>
+                                                <td><a href='user_edit.php?tipoUsuario=responsavel&nome=$reg->nomeResponsavel&cod=$reg->codResponsavel'>alterar dados</a></td>
+                                             </tr>";
+                                        echo "teste";
+                                        $c++;
+                                    }
+                                    
+                                }else{
+                                    echo "Erro na tabela do banco de dados! tente novamente mais tarde ;/";
+                                }
+                                
+                            }echo "</table>";
+                        }
+                    }else{
+                        echo "<a href='user_view.php?tipoSelect=aluno'>Usuários alunos ||</a>
+                             <a href='user_view.php?tipoSelect=professor'>Usuários professores ||</a>
+                             <a href='user_view.php?tipoSelect=responsavel'>Usuários responsavel </a>
+                             <br><br>Nenhum usuário cadastrado no momento; ";
+                    }
+                }else{
+                    echo "erro no banco de dados! tente novamente mais tarde :/";
+                }
+                
+            }else{
+                echo "Somente administradores tem acesso a essa página :/";
+            }
+
+        ?>
+        <br><Br><a href="index.php">Voltar</a>
+    </body>
+</docytipe>
